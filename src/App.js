99d02af3616dd4f80735baa201faf3df86d0a23f
MOHIT-1911 <mohit1911.mp@gmail.com>
2022-11-16 19:55:08 +0530

    forth

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import Header from "./components/Header";
 import { BrowserRouter, Route,Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
+import {Helmet} from "react-helmet";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <>
+        <Helmet>
+                <meta charSet="utf-8" />
+                <title>BuyNow</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+         </Helmet>
+        <BrowserRouter>
+        <Header />
 
-      <div className="App">
+        <div className="App">
         <Routes>
-            <Route path="/" exact element={<Home />}/>
-            <Route path="/cart" exact element={<Cart />}/>
+        <Route path="/" exact element={<Home />}/>
+        <Route path="/cart" exact element={<Cart />}/>
         </Routes>
-       
-      </div>
-    </BrowserRouter>
+
+        </div>
+        </BrowserRouter>
+    </>
+      
   );
 }
 
